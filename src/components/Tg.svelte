bad85581531f73acf12962cba11bb8c259ced9dc
Evgeniya Pavlova <evgeniya.pavlova@quadcode.com>
2025-04-20 11:12:59 +0300

    add site

---
+++ src/components/Tg.svelte
@@ -0,0 +1,7 @@
+<script>
+	import TelegramLogo from './TelegramLogo.svelte';
+</script>
+
+<a class="tg-block" id="tg-block" href="https://t.me/Traderneider">
+	<TelegramLogo /> Any other questions?
+</a>
